dc0705ff069bdd64defb3576f00f9f7e9b42004b
Venkat2003v <128215579+Venkat2003v@users.noreply.github.com>
2024-03-02 07:17:42 +0530

    Delete netflix_clone directory

--- netflix_clone/Signout/footer1.js
+++
@@ -1,3 +0,0 @@
-import footer from "../component/footer.js";
-console.log(footer)
-document.getElementById("mainfooter").innerHTML=footer();
--- netflix_clone/Signup/footer2.js
+++
@@ -1,4 +0,0 @@
-// console.log("gfdgfg")
-import footer from "../component/footer.js";
-// console.log(footer);
-document.getElementById("mainfooter").innerHTML=footer();
